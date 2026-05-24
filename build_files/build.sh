1529a8b30b332800daa7f5fb53a145a7b37dccc3
sapmelonnnn <theeradechguide@gmail.com>
2026-05-25 02:31:30 +0700

    niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,9 +9,56 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y copr enable bieszczaders/kernel-cachyos
+rpm -e $(rpm -qa | grep '^kernel-core') --nodeps
+dnf5 -y install kernel-cachyos-headers
+dnf5 -y copr disable bieszczaders/kernel-cachyos
 
+# Install Quickshell + dependencies
+dnf5 -y copr enable errornointernet/quickshell
+dnf5 -y install quickshell
+dnf5 -y copr disable errornointernet/quickshell
+
+# Noctalia dependencies
+dnf5 -y copr enable wef/cliphist
+dnf5 -y install cliphist
+dnf5 -y copr disable wef/cliphist
+
+dnf5 -y copr enable heus-sueh/packages
+dnf5 -y install matugen
+dnf5 -y copr disable heus-sueh/packages
+
+# this installs a package from fedora repos
+dnf5 install -y \
+  niri \
+  xwayland-satellite \
+  hyprpaper \
+  wl-clipboard \
+  brightnessctl \
+  cava \
+  rofi-wayland \
+  fd-find \
+  ripgrep \
+  fzf \
+  zoxide \
+  kitty \
+  thunar \
+  gvfs \
+  gvfs-mtp \
+  tumbler \
+  file-roller \
+  grim \
+  slurp \
+  playerctl \
+  network-manager-applet \
+  polkit-gnome \
+  thunar-volman \
+  thunar-archive-plugin \
+  pavucontrol \
+  noto-fonts-emoji \
+  papirus-icon-theme \
+  sddm \
+  xdg-desktop-portal-gtk
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -21,4 +68,5 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
+systemctl enable sddm
 systemctl enable podman.socket
